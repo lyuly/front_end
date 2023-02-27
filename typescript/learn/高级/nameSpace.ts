97c4d9093999fbed43fb312342074515ce94d949
lyuly <liumike567@gmail.com>
2023-02-27 15:12:22 +0800

    update scss el

--- typescript/learn/高级/nameSpace.ts
+++ typescript/learn/高级/nameSpace.ts
@@ -1,24 +1,39 @@
-namespace Use {
-  export function log(msg: any) {
-    console.log(msg)
+export type UseNamespace = {
+  b: () => string;
+  e: (el: string) => string;
+  m: (mo: string) => string;
+  em: (el: string, mo: string) => string;
+}
+
+function createPrefix(namespace: string, element?: string, modifier?: string): string {
+  let cls = namespace
+  if (element) {
+    cls += `__${element}`;
   }
-  export function error(msg: any) {
-    console.error(msg)
+  if (modifier) {
+    cls += `--${modifier}`;
+  }
+  return cls
+}
+
+export function useNamespace(block: string, needDot = false): UseNamespace {
+  const namespace = needDot ? `.wviewui-${block}` : `wviewui-${block}`;
+  const b = () => createPrefix(namespace);
+  const e = (element: string) => (element ? createPrefix(namespace, element) : '');
+  const m = (modifier: string) => (modifier ? createPrefix(namespace, modifier) : '');
+  const em = (element: string, modifier: string) => (element && modifier ? createPrefix(namespace, element, modifier) : '')
+  return {
+    b,
+    e,
+    m,
+    em
   }
 }
 
-Use.log('call me')
-Use.error('ok')
-
-namespace Hello {
-  export function log(msg: string) {
-    console.log(msg)
-  }
-
-  export function info(msg: string) {
-    console.info(msg)
-  }
-}
-
-Hello.log('World')
-Hello.info('Windy')
+/* 
+const ns = useNamespace('button')
+ns.b() // wviewui-button
+ns.e('hello') // wview-button_hello
+ns.m('hello') // wview-button--hello
+ns.em('hello', 'world') // wview-button__hello--world
+*/
